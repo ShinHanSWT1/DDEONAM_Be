657d4f68801e7979eecc59076c2f133850f37a5c
YEJIN <leeyjin0926@gmail.com>
2026-03-23 01:08:26 +0900

    init project

---
+++ src/main/java/com/gorani/ddeonam/DdeonamBeApplication.java
@@ -0,0 +1,13 @@
+package com.gorani.ddeonam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdeonamBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdeonamBeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/gorani/ddeonam/sample/controller/SampleController.java
@@ -0,0 +1,15 @@
+package com.gorani.ddeonam.sample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "ok";
+    }
+}
