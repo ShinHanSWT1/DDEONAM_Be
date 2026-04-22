1386aee68614c5e8ef82eb576131212ffde768c2
wlaos <dkdlfjsl0719@gmail.com>
2026-04-22 10:58:58 +0900

    feat: 신한오일뱅크 쿠폰 추가

---
+++ src/main/resources/db/migration/V22__add_shinhan_oilbank_coupon_template.sql
@@ -0,0 +1,2 @@
+INSERT INTO coupon_templates (category, name, discount_label, valid_days, is_pay_usable, status, created_at)
+VALUES ('fuel', '신한오일뱅크 5,000원 주유 할인권', '5,000원', 30, true, 'ACTIVE', NOW());
